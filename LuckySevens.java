7b4be63e45595d1dc52ea297504516ef41648651
jared <jared.collacchi@gmail.com>
2021-01-13 16:57:57 -0500

    1

--- LuckySevens.java
+++ LuckySevens.java
@@ -4,8 +4,48 @@
  *  @author: Your Name
  *  @AP Computer Science A, Virtual Virginia
  */
+import java.util.Random; 
 
 public class LuckySevens
 {
+    private int balance;
+    private int numOfTurns;
     
+    //return​a randomly generated integer from a standard​die
+    static int rollDie() {
+        
+        Random rand = new Random();
+        
+        int rand_die = rand.nextInt(6) + 1;
+        
+        return rand_die;
+    }
+    
+    static int playTurn() {
+        int dieOne, dieTwo, totalDie;
+        
+        dieOne = rollDie();
+        dieTwo = rollDie();
+        totalDie = dieOne + dieTwo;
+        
+        return totalDie;
+    }
+  
+  
+  
+    static void playGame() {
+        // code to be executed
+    }
+    static void getGameReport() {
+        // code to be executed
+    }
+    public int getNumOfTurns() {
+        return playTurn();
+    }
+    static void getBalance() {
+        // code to be executed
+    }
+
 }
+
+
